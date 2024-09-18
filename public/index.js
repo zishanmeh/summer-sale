a1ba3527c6497056e9f7437d48dd126b5f5d47f4
zishanmeh <zishanmehedihasan2@gmail.com>
2024-09-18 01:48:31 +0100

    file path bug attempt

--- public/index.js
+++ public/index.js
@@ -3,7 +3,7 @@ let discount = 0;
 let total_price = 0;
 
 async function fetchData() {
-  const response = await fetch("data/products.json");
+  const response = await fetch("./././data/products.json");
   //   D:\Programming hero course\Summer sale\summer-sale\data\products.json
   //   data\products.json
   //
